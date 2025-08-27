030106c8c3f4453d558f233130937dd6dc5b390a
eurorondon03 <eurorondon03@gmail.com>
2025-08-27 20:03:22 +0200

    quitando limpieza de cortesia en creacion de reserva Cliente

--- frontend/views/site/createN.php
+++ frontend/views/site/createN.php
@@ -465,6 +465,9 @@ Modal::end();
             <div style="display:<?= $model->factura == 1 ? 'none !important' : '' ?>">
               <?php
               foreach ($servicios as $s) {
+                if (in_array($s->id, [7])) {
+                  continue; // omitir “Limpieza de cortesía”, “Plaza reservada” y “Techado”
+                }
                 $service = array($s->id => $s->nombre_servicio);
                 $checked = "";
 
