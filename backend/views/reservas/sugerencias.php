be1a9c5b0c74e71ef638e7bbc952b21e3b5d8658
eurorondon <91265882+eurorondon@users.noreply.github.com>
2025-10-02 17:45:25 +0200

    Add suggestion totals to reservas suggestions view

--- backend/views/reservas/sugerencias.php
+++ backend/views/reservas/sugerencias.php
@@ -6,6 +6,8 @@ use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\EncuestaInicialSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $totalPositivas int */
+/* @var $totalNegativas int */
 
 $this->title = 'Sugerencias de Clientes';
 ?>
@@ -13,6 +15,12 @@ $this->title = 'Sugerencias de Clientes';
   <div class="panel panel-default panel-index">
     <div class="panel-heading caja-title">Sugerencias de Clientes</div>
     <div class="panel-body gs1">
+      <div class="alert alert-info">
+        <strong>Resumen global:</strong>
+        <span>Sugerencias positivas: <?= Html::encode($totalPositivas) ?></span>
+        <span class="m-l-15">Sugerencias negativas: <?= Html::encode($totalNegativas) ?></span>
+      </div>
+      <p class="help-block">Los totales mostrados corresponden al acumulado general de encuestas, independientemente de los filtros aplicados.</p>
       <?php Pjax::begin(); ?>
       <?= GridView::widget([
         'dataProvider' => $dataProvider,
