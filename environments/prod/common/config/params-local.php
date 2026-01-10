22952fcb1005e6f2fe74276a787d4fdc20b1cc61
eurorondon03 <eurorondon03@gmail.com>
2026-01-10 16:51:36 +0100

    v2

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,11 +1,2 @@
 <?php
-return [
-    'redsys' => [
-        'fuc' => '350165395',
-        'terminal' => '001',
-        'currency' => '978',
-        'paymentUrl' => 'https://sis-t.redsys.es:25443/sis/realizarPago',
-        'testMaxAmount' => 5.00,
-        'merchantKey' => 'sq7HjrUOBfKmC576ILgskD5srU870gJ7',
-    ],
-];
+return [];
