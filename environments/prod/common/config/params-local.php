e92f29b629ea12d49ff36d10f8c9482d422bd03e
eurorondon <91265882+eurorondon@users.noreply.github.com>
2026-01-10 16:15:22 +0100

    Move Redsys config to local params

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,3 +1,10 @@
 <?php
 return [
+    'redsys' => [
+        'fuc' => '350165395',
+        'terminal' => '001',
+        'currency' => '978',
+        'paymentUrl' => 'https://sis-t.redsys.es:25443/sis/realizarPago',
+        'testMaxAmount' => 5.00,
+    ],
 ];
